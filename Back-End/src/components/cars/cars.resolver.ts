2199027dc870e25d3e1b5609a655988c0d5833c0
abdolazizsalimi <aziz.s6882@gmail.com>
2023-06-20 20:45:15 +0330

    add :[Cas sevices and resolver function to add and get cars ]

--- Back-End/src/components/cars/cars.resolver.ts
+++ Back-End/src/components/cars/cars.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CarsService } from './cars.service';
 import { Car } from './entities/car';
+import { NewCarInput } from './dto/new-car.input';
 
 
 @Resolver()
@@ -12,6 +13,15 @@ export class CarsResolver {
         return await this.carService.getAllCars().catch((err)=>{
             throw err;
         });
+    }
+
+    @Mutation((returns) => Car)
+    public async addNewCar(
+      @Args('newCarData') newCarData: NewCarInput,
+    ): Promise<Car> {
+      return await this.carService.addCar(newCarData).catch((err) => {
+        throw err;
+      });
     }
 
 
