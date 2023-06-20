55cba957a3d20adf5747615b6b61e7288bd4069d
abdolazizsalimi <aziz.s6882@gmail.com>
2023-06-20 20:29:20 +0330

    add :[Cars DTO and cars get all]

--- Back-End/src/components/cars/cars.resolver.ts
+++ Back-End/src/components/cars/cars.resolver.ts
@@ -1,14 +1,17 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { CarsService } from './cars.service';
+import { Car } from './entities/car';
 
 
 @Resolver()
 export class CarsResolver {
     constructor(private carService : CarsService){}
 
-    @Query((returns) => String)
-    public async cars(){
-        return "Hello and welcome cars App! ";
+    @Query((returns) => [Car])
+    public async cars() : Promise<Car[]>{
+        return await this.carService.getAllCars().catch((err)=>{
+            throw err;
+        });
     }
 
 
