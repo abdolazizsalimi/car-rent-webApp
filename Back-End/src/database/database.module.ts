138f5ed0049e0d5102f2dbb52124dc736f9210fc
abdolazizsalimi <aziz.s6882@gmail.com>
2023-06-24 14:49:52 +0330

    [finished v.1]

--- Back-End/src/database/database.module.ts
+++ Back-End/src/database/database.module.ts
@@ -3,32 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection, getConnectionOptions } from 'typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(
-
-    {
-      "type": "mysql",
-      "host": "localhost",
-      "port": 3306,
-      "username": "root",
-      "password": "aziz6882",
-      "database": "yourcar",
-      "entities": ["dist/**/entities/*{.ts,.js}"],
-      "synchronize": true,
-      "migrations": ["dist/migrations/*{.ts,.js}"]
-
-    }
-
-  )
-  //   {
-    // useFactory: async () =>
-    // Object.assign(
-    //   await getConnectionOptions(
-    //     process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
-    //   ),
-    // ),
-  // }
-    
-  // )
+  imports: [TypeOrmModule.forRootAsync({
+    useFactory: async () =>
+    Object.assign(
+      await getConnectionOptions(
+        process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
+      ),
+    ),
+  })
 ],
   exports: [TypeOrmModule]
 })
